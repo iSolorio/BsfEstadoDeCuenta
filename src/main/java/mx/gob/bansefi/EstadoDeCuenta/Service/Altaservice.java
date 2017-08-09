74f0e601dab7a1a718cc0a0de732aee636afbfe5
Juan Carlos <leotecisc@gmail.com>
2017-08-09 16:34:41 -0500

    Se añanade jasperr y modifica DTO para enviar datos hardcodeados

--- src/main/java/mx/gob/bansefi/EstadoDeCuenta/Service/Altaservice.java
+++ src/main/java/mx/gob/bansefi/EstadoDeCuenta/Service/Altaservice.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-
 import mx.gob.bansefi.EstadoDeCuenta.DB.ManejoDB;
 import mx.gob.bansefi.EstadoDeCuenta.dto.RequestGralDTO;
 import mx.gob.bansefi.EstadoDeCuenta.dto.jasperDTO;
@@ -35,35 +34,38 @@ public class Altaservice {
 	private Util util;
 	@Value("${url.database}")
 	private String urlDataBase;
- /* servicio basico de prueba*/
+	/* servicio basico de prueba */
 	public static byte[] report;
 	ByteArrayOutputStream out = new ByteArrayOutputStream();
-	public void altaEstado(RequestGralDTO request)
-	{
-		
+
+	public void altaEstado(RequestGralDTO request) {
+
 		System.out.println("Funciona");
-		//util.callRestPost( request, uri);
-		Connection con=manejodb.dbConnect();
-		ArrayList<jasperDTO> datos=new ArrayList<jasperDTO>();
+		// util.callRestPost( request, uri);
+		Connection con = manejodb.dbConnect();
+		ArrayList<jasperDTO> datos = new ArrayList<jasperDTO>();
 		List<jasperDTO> lista = new ArrayList<jasperDTO>();
-		lista.add(new jasperDTO("1","2","3"));
-		
+		// lista.add(new jasperDTO("1", "2", "3"));
+		lista.add(new jasperDTO("Juan Carlos", "Sta Rosalia", "ABCD123456ABCV1", "08/02/2017", "08/04/2017", "59", "08/04/2017", "$2384.14", "$0.00", "3", "$165.25", "$185.15", "08/02/2017", "08/04/2017", "$2384", "08/02/2017", "08/04/2017", "$30.03", "$0.00", "$0.00", "$0.84", "$2384",
+				"CREDITO MAS AHORRO", "0349176172", "1003 Sucursal Empresarial", "40.00%", "Pesos Mexicanos", "$2500", "$0.00", "08/12/2019", "009.990%", "00.00%", "PERIODOS REALIES", "PERIODOS RELAES", "$0.00", "08/04/2017", "08/04/2017", "Facturacion capital", "$125.88", "", "", "", "", "", ""));
+
 		Map<String, Object> parametros = new HashMap<String, Object>();
-		//parametros.put("Descripcion",lista.get(0).getDescripcion());
-		//datos.add(new jasperDTO("4","5","6"));
+		// parametros.put("Descripcion",lista.get(0).getDescripcion());
+		// datos.add(new jasperDTO("4","5","6"));
 		try {
-			JasperReport jasperReport = (JasperReport) JRLoader.loadObjectFromFile("nada.jasper");
-			  JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parametros,new JRBeanCollectionDataSource(lista));
-			  JasperExportManager.exportReportToPdfStream(jasperPrint, out);
-			  report=out.toByteArray();
-			  JasperExportManager.exportReportToPdfFile(jasperPrint, "C:\\Users\\AppWhere\\Documents\\proyectos\\BsfEstadoDeCuenta\\reporteejemplo.pdf");
-			    manejodb.insertPDF(con, "C:\\Users\\AppWhere\\Documents\\plantillaestadodecuenta.pdf",report);
-				manejodb.getPDFData(con);
+			JasperReport jasperReport = (JasperReport) JRLoader.loadObjectFromFile("EstadoDeCuenta.jasper");
+			JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parametros,
+					new JRBeanCollectionDataSource(lista));
+			JasperExportManager.exportReportToPdfStream(jasperPrint, out);
+			report = out.toByteArray();
+			JasperExportManager.exportReportToPdfFile(jasperPrint,
+					"C:\\Users\\appWhere\\Documents\\Proyecto\\BsfEstadoDeCuenta\\reporteejemplo.pdf");
+			manejodb.insertPDF(con, "C:\\Users\\AppWhere\\Documents\\plantillaestadodecuenta.pdf", report);
+			manejodb.getPDFData(con);
 		} catch (JRException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-	  
+
 	}
 }
--- src/main/java/mx/gob/bansefi/EstadoDeCuenta/dto/jasperDTO.java
+++ src/main/java/mx/gob/bansefi/EstadoDeCuenta/dto/jasperDTO.java
@@ -9,10 +9,101 @@ import lombok.Setter;
 @NoArgsConstructor
 public class jasperDTO {
 
-	@Getter @Setter
+	/*@Getter @Setter
 	private String idInterno;
 	@Getter @Setter
 	private String cuenta;
 	@Getter @Setter
+	private String descripcion;*/
+	
+	@Getter @Setter
+	private String nombre;
+	@Getter @Setter
+	private String domicilio;
+	@Getter @Setter
+	private String rfc;
+	@Getter @Setter
+	private String periodo1;
+	@Getter @Setter
+	private String periodo2;
+	@Getter @Setter
+	private String numDias;
+	@Getter @Setter
+	private String fechaCorte;
+	@Getter @Setter
+	private String saldoInicial;
+	@Getter @Setter
+	private String dispEnPeriodo;
+	@Getter @Setter
+	private String numAbonos;
+	@Getter @Setter
+	private String montoDeAbonos;
+	@Getter @Setter
+	private String capitalFact;
+	@Getter @Setter
+	private String baseIntOrd;
+	@Getter @Setter
+	private String baseIntOrd1;
+	@Getter @Setter
+	private String baseIntOrd2;
+	@Getter @Setter
+	private String intOrd;
+	@Getter @Setter
+	private String intOrd1;
+	@Getter @Setter
+	private String intOrd2;
+	@Getter @Setter
+	private String intMoraPer;
+	@Getter @Setter
+	private String comisionPer;
+	@Getter @Setter
+	private String ivaPer;
+	@Getter @Setter
+	private String saldoFinal;
+	@Getter @Setter
+	private String tipoCta;
+	@Getter @Setter
+	private String numDeCredito;
+	@Getter @Setter
+	private String sucursal;
+	@Getter @Setter
+	private String cat;
+	@Getter @Setter
+	private String moneda;
+	@Getter @Setter
+	private String lineaDeCred;
+	@Getter @Setter
+	private String disLineaDeCred;
+	@Getter @Setter
+	private String fechaDeVenc;
+	@Getter @Setter
+	private String tasaIntOrd;
+	@Getter @Setter
+	private String tasaIntMor;
+	@Getter @Setter
+	private String formaPago;
+	@Getter @Setter
+	private String pagoDeInt;
+	@Getter @Setter
+	private String totalPagar;
+	@Getter @Setter
+	private String fechaLimite;
+	@Getter @Setter
+	private String fechaOp;
+	@Getter @Setter
+	private String conceptoOp;
+	@Getter @Setter
+	private String cargoOp;
+	@Getter @Setter
+	private String abonoOp;
+	@Getter @Setter
+	private String folioAclaracion;
+	@Getter @Setter
+	private String numDeCta;
+	@Getter @Setter
+	private String fechaCargo;
+	@Getter @Setter
 	private String descripcion;
+	@Getter @Setter
+	private String cargo;
 }
